99a2ae0cd29ccb3040d07251d25bace6f53d82ab
Wyatt Winters <git@wyattwinters.com>
2013-07-08 18:15:18 -0500

    Now includes weather info using Flask, not my horrible hack

--- main.py
+++ main.py
@@ -8,25 +8,20 @@ app = Flask(__name__)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 def getWeather():
-	result = pywapi.get_weather_from_yahoo( "37216" , units = 'imperial' )
-	f = open("static/weather.js", "w")
-	string = result['html_description']
-	string = string.replace("\n", "")
-	string = string.replace("(provided by <a href=\"http://www.weather.com\" >The Weather Channel</a>)<br/>", "")
-	string = string.replace("<br /><a href=\"http://us.rd.yahoo.com/dailynews/rss/weather/Nashville__TN/*http://weather.yahoo.com/forecast/USTN0357_f.html\">Full Forecast at Yahoo! Weather</a><BR/><BR/>", "")
-	f.write("document.write('" + string + "');")
-	f.close()
-
-@app.before_request
-def before_request():
-	getWeather()
+    result = pywapi.get_weather_from_yahoo( "37216" , units = 'imperial' )
+    string = result['html_description']
+    string = string.replace("\n", "")
+    string = string.replace("(provided by <a href=\"http://www.weather.com\" >The Weather Channel</a>)<br/>", "")
+    string = string.replace("<br /><a href=\"http://us.rd.yahoo.com/dailynews/rss/weather/Nashville__TN/*http://weather.yahoo.com/forecast/USTN0357_f.html\">Full Forecast at Yahoo! Weather</a><BR/><BR/>", "")
+    return string
 
 @app.route('/')
 def my_form():
     f = open("set_temp", "r")
     targetTemp = f.readline()
     f.close()
-    return render_template("form.html", targetTemp = targetTemp )
+    weatherString = getWeather()
+    return render_template("form.html", targetTemp = targetTemp, weatherString = weatherString)
 
 @app.route("/", methods=['POST'])
 def my_form_post():
@@ -40,4 +35,4 @@ def my_form_post():
     return redirect(url_for('my_form'))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run()
