7db83fa294ec8ca0b41b1c800e8c462f8cc114d1
Wyatt Winters <git@wyattwinters.com>
2013-07-09 05:20:15 -0500

    I'll find YOUR files!

--- daemon.py
+++ daemon.py
@@ -25,24 +25,24 @@ while 1 == 1:
     if outdoor_temp < set_temp:
         if hvac_state == 0: #idle
             if indoor_temp < set_temp - inactive_hysteresis:
-                os.system("hvac_heat.sh")
+                os.system("./hvac_heat.sh")
                 hvac_state = 1
         elif hvac_state == 1: #heating
             if indoor_temp > set_temp + active_hysteresis:
-                os.system("hvac_idle.sh")
+                os.system("./hvac_idle.sh")
         elif hvac_state == -1: # it's cold out, why is the AC running?
-                os.system("hvac_idle.sh")
+                os.system("./hvac_idle.sh")
     # ac mode
     else:
         if hvac_state == 0: #idle
             if indoor_temp > set_temp + inactive_hysteresis:
-                os.system("hvac_cool.sh")
+                os.system("./hvac_cool.sh")
                 hvac_state = -1
         elif hvac_state == -1: #cooling
             if indoor_temp < set_temp - active_hysteresis:
-                os.system("hvac_idle.sh")
+                os.system("./hvac_idle.sh")
                 hvac_state = 0
         elif hvac_state == 1: # it's hot out, why is the heater on?
-                os.system("hvac_idle.sh")
+                os.system("./hvac_idle.sh")
 
     time.sleep(10)
