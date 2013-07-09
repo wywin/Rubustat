61341d832f9e7f09b5fc9a325cc443cab2e5f3f9
Wyatt Winters <git@wyattwinters.com>
2013-07-09 05:39:57 -0500

    now floaty, and less buggy!

--- daemon.py
+++ daemon.py
@@ -12,14 +12,14 @@ os.chdir(os.getcwd())
 while 1 == 1:
 
 
-    indoor_temp = subprocess.check_output("./get_indoor_temp.sh")
-    outdoor_temp = subprocess.check_output("./get_outdoor_temp.sh")
+    indoor_temp = float(subprocess.check_output("./get_indoor_temp.sh"))
+    outdoor_temp = float(subprocess.check_output("./get_outdoor_temp.sh"))
 
     file = open("set_temp", "r")
     set_temp = float(file.readline())
     file.close()
      
-    hvac_state = int(subprocess.check_output("./get_hvac_state.sh"))
+    hvac_state = float(subprocess.check_output("./get_hvac_state.sh"))
     
     # heater mode
     if outdoor_temp < set_temp:
@@ -45,4 +45,10 @@ while 1 == 1:
         elif hvac_state == 1: # it's hot out, why is the heater on?
                 os.system("./hvac_idle.sh")
 
+    #debug stuff
+    print "Sleepy time!"
+    print "hvac_state = " + str(hvac_state)
+    print "indoor_temp = " + str(indoor_temp)
+    print "outdoor_temp = " + str(outdoor_temp)
+    print "set_temp = " + str(set_temp)
     time.sleep(10)
