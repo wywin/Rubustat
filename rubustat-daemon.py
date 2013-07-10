397495d87ed8ea321e3fdea4e7529ec8ce99e8e6
Wyatt Winters <git@wyattwinters.com>
2013-07-10 10:36:52 -0500

    you gotta tell it to use a shell!

--- rubustat-daemon.py
+++ rubustat-daemon.py
@@ -15,9 +15,9 @@ DEBUG = 0
 while 1 == 1:
 
 
-    indoor_temp = float(subprocess.check_output("./etc.sh indoor_temp"))
-    outdoor_temp = float(subprocess.check_output("./etc.sh outdoor_temp"))
-    hvac_state = int(subprocess.check_output("./etc.sh get_state"))
+    indoor_temp = float(subprocess.check_output("./etc.sh indoor_temp", shell=True))
+    outdoor_temp = float(subprocess.check_output("./etc.sh outdoor_temp", shell=True))
+    hvac_state = int(subprocess.check_output("./etc.sh get_state", shell=True))
 
     file = open("set_temp", "r")
     set_temp = float(file.readline())
@@ -27,30 +27,30 @@ while 1 == 1:
     if outdoor_temp < set_temp:
         if hvac_state == 0: #idle
             if indoor_temp < set_temp - inactive_hysteresis:
-                os.system("./etc.sh heat")
+                os.system("./etc.sh heat", shell=True)
                 hvac_state = 1
         elif hvac_state == 1: #heating
             if indoor_temp > set_temp + active_hysteresis:
-                os.system("./etc.sh fan_to_idle") 
+                os.system("./etc.sh fan_to_idle", shell=True) 
                 time.sleep(300)
-                os.system("./etc.sh idle")
+                os.system("./etc.sh idle", shell=True)
                 hvac_state = 0
         elif hvac_state == -1: # it's cold out, why is the AC running?
-                os.system("./etc.sh idle")
+                os.system("./etc.sh idle", shell=True)
     # ac mode
     else:
         if hvac_state == 0: #idle
             if indoor_temp > set_temp + inactive_hysteresis:
-                os.system("./etc.sh cool")
+                os.system("./etc.sh cool", shell=True)
                 hvac_state = -1
         elif hvac_state == -1: #cooling
             if indoor_temp < set_temp - active_hysteresis:
-                os.system("./etc.sh fan_to_idle") 
+                os.system("./etc.sh fan_to_idle", shell=True) 
                 time.sleep(300)
-                os.system("./etc.sh idle")
+                os.system("./etc.sh idle", shell=True)
                 hvac_state = 0
         elif hvac_state == 1: # it's hot out, why is the heater on?
-                os.system("./etc.sh idle")
+                os.system("./etc.sh idle", shell=True)
 
     #debug stuff
     if DEBUG == 1:
