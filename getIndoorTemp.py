d27ea301730aa4a6f81a1119acfe2bd3fc41966a
Wyatt Winters <git@wyattwinters.com>
2013-07-15 18:20:41 -0500

    Why is the preservation of formatting when copy pasting so inconsistent across editors?

--- getIndoorTemp.py
+++ getIndoorTemp.py
@@ -15,14 +15,14 @@ def read_temp_raw():
 def read_temp():
     lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
-    time.sleep(0.2)
-    lines = read_temp_raw()
+    	time.sleep(0.2)
+    	lines = read_temp_raw()
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
-    temp_string = lines[1][equals_pos+2:]
-    temp_c = float(temp_string) / 1000.0
-    temp_f = temp_c * 9.0 / 5.0 + 32.0
-    return temp_f
+    	temp_string = lines[1][equals_pos+2:]
+    	temp_c = float(temp_string) / 1000.0
+    	temp_f = temp_c * 9.0 / 5.0 + 32.0
+    	return temp_f
     
 
 def getIndoorTemp():
