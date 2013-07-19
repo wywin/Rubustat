46907c9dd71424967b45633f55a2971a29f5a9fd
Wyatt Winters <git@wyattwinters.com>
2013-07-19 14:58:58 -0500

    restore RPI specific commands (GPIO, etc)

--- getIndoorTemp.py
+++ getIndoorTemp.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-#Based off the tutorial by adafruit here: 
+#Based off the tutorial by adafruit here:
 # http://learn.adafruit.com/adafruits-raspberry-pi-lesson-11-ds18b20-temperature-sensing/software
 
 import os
@@ -7,7 +7,25 @@ import glob
 import time
 
 def getIndoorTemp():
-	
-    return 70
+
+    os.system('modprobe w1-gpio')
+    os.system('modprobe w1-therm')
+    base_dir = '/sys/bus/w1/devices/'
+    device_folder = glob.glob(base_dir + '28*')[0]
+    device_file = device_folder + '/w1_slave'
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        f = open(device_file, 'r')
+        lines = f.readlines()
+        f.close()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_f
      
-    
+    return read_temp()
