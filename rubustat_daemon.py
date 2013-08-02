cb2a1f5cda7fdfeaededfe1a9787b5cbe8bf1c4a
Wyatt Winters <git@wyattwinters.com>
2013-08-01 19:17:31 -0500

    fix typo

--- rubustat_daemon.py
+++ rubustat_daemon.py
@@ -29,7 +29,7 @@ GPIO.setup(HEATER_PIN, GPIO.OUT)
 GPIO.setup(AC_PIN, GPIO.OUT)
 GPIO.setup(FAN_PIN, GPIO.OUT)
 os.popen("echo " + str(HEATER_PIN) + " > /sys/class/gpio/export")
-os.popen("echo " + str(COOL_PIN) + " > /sys/class/gpio/export")
+os.popen("echo " + str(AC_PIN) + " > /sys/class/gpio/export")
 os.popen("echo " + str(FAN_PIN) + " > /sys/class/gpio/export")
 
 ###Begin helper functions###
